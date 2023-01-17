b751b6c969769ca4d383c5df20c4f34d7e646e28
SUBID DAS <itsmesubid@gmail.com>
2023-01-17 19:00:24 +0530

    Add `Anagram program`

---
+++ anagram.js
@@ -0,0 +1,14 @@
+let first = "hello";
+let second = "llleo";
+
+const isAnagram = (first, second) => {
+    if (first.length !== second.length) return false;
+    else {
+        for (let element of first) {
+            if (!second.includes(element)) return false;
+        }
+        return true;
+    }
+}
+
+console.log(isAnagram(first, second) ? "Anagram" : "Not Anagram");
